2121c670a6532926e8da9671169349b2548ab703
Lucas Silva <martssilvapro@gmail.com>
2021-06-24 18:37:49 -0300

    FUNCIONOU AEEEEEW

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const express = require('express');
 
 const app = express();
+
+const recipes = [
+  {
+    id: 12345,
+    name: 'farofa de bacon',
+    ingredients: ['farofa', 'bacon']
+  },
+  {
+    id: 12346,
+    name: 'ovo mexido',
+    ingredients: ['ovo']
+  }
+]
+
+app.delete('/recipe/:id', (req, res) => {
+  const result = deleteRecipes(req.params.id);
+
+  res.send(result);
+});
+
+
+function deleteRecipes(id) {
+  const result = recipes.find((i) => i.id === parseInt(id, 10));
+
+  if(!result) {
+    return 'recipe not found'
+  } else {
+    return result
+  }
+}
+
+app.listen(3001, () => {
+  console.log('Hello');
+});
